52c38d6824731082f3cc34aa44ccb165a42a7767
rurushu <olymangue@gmail.com>
2022-03-09 17:16:43 +0200

    continua dev

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,169 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Link from 'next/link'
-import logo from '../assets/logo.png'
-import twitter from '../public/twitter.png'
-import facebook from '../public/facebook.png'
-import linkedin from '../public/linkedin.png'
-import youtube from '../public/youtube.png'
-import instagram from '../public/instagram.png'
-import cover from '../public/mozdevz-cover.jpg'
-import { ComingSoon, Header, Main, SideCover, SocialIconWrapper } from '../styles/comingSoon'
+import { HomePage } from '../styles/home';
+import cover from '../assets/cover.svg';
+import codingIcon from '../assets/icons/programing.svg';
+import mozdevz from '../assets/mozdevz.jpg';
+import Header from '../components/Header';
+import CardContainer from '../components/CardContainer';
+import Footer from '../components/Footer'
+import LogoContainer from '../components/LogoContainer';
+import StatsItem from '../components/StatsItem';
+
+import { Button } from '../components/Button';
 
 export default function Home() {
   return (
-    <ComingSoon>
+    <HomePage>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#e2363c" />
+        <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="A maior comunidade de devz em Moz"
         />
-        <title>Em manutenção</title>
+        <title>Comunidade Mozdevz</title>
       </Head>
-      
-      {/* Header Section */}
-      <Header>
-        <Image 
-          src={logo}
-          alt='Comunidade Moçambicana de Desenvolvedores de Software e Aplicações'
-        />
-      </Header>
+      <Header /> 
+      <section className="hero">
+        {/* Description */}
+        <aside className="heroDescription">
+          <h1 className="heroTitle">
+            A maior comunidade de desenvolvedores de Moçambique
+          </h1>
+          <Button>
+            Saiba Mais
+          </Button>
+        </aside>
 
-      {/* Hero Image Section */}
-      <SideCover>
-        <div className='img-mobile'>
-          <Image 
+        {/* Cover */}
+        <aside className="heroCover">
+          <Image
+            className="coverImage"
             src={cover}
-            alt='Mozdevz hero mobile'
+            alt='Comunidade Mozdevz'
+            width={540}
+            height={390}
           />
-        </div>
-        <div className='img-desktop'>
-          <Image 
-            src={cover}
-            alt='Mozdevz hero desktop'
-            layout='fill'
-            objectFit='cover'
-          />
-        </div>
-      </SideCover>
+        </aside>
+      </section>
 
-      {/* Main Content Section */}
-      <Main>
-        <h1>Estamos em <span>manutenção</span></h1>
-        <h2>Segue-nos para <br /> mais actualizações!</h2>
-        <p>contacto@mozdevz.org</p>
-       
-       {/* Socal Media Icons */}
-       <SocialIconWrapper>
-          <Link href="https://twitter.com/mozdevz">
-            <a className="icon-wrapper">
-              <Image src={twitter} alt='twitter icon'/>
-            </a>
-          </Link>
-          <Link href="https://www.facebook.com/mozdevz/">
-            <a className="icon-wrapper">
-              <Image src={facebook} alt='facebook icon'/>
-            </a>
-          </Link>
-          <Link href="https://www.linkedin.com/company/mozdevz/">
-            <a className="icon-wrapper">
-              <Image src={linkedin} alt='linkedin icon'/>  
-            </a>
-          </Link>
-          <Link href="https://www.instagram.com/mozdevz/">
-            <a className="icon-wrapper">
-              <Image src={instagram} alt='instagram icon'/>  
-            </a>
-          </Link>
-          <Link href="https://www.youtube.com/channel/UC4vjNsLZPZUYTjsQllEMiMQ">
-            <a className="icon-wrapper">
-              <Image src={youtube} alt='youtube icon'/>  
-            </a>
-          </Link>
-        </SocialIconWrapper>
-      </Main>
-    </ComingSoon>
+      <section className="stats">
+        {/* Description */}
+        <aside className="statsDescription">
+          <h2 className="statsTitle">
+            Impactamos milhares de
+            <span className="hightlight"> devz </span>
+            e impulsionamos carreiras
+          </h2>
+          <i className="codingIcon">
+            <Image
+              src={codingIcon}
+              alt='Coding Icon'
+              width={90}
+              height={90}
+            />
+          </i>
+        </aside>
+
+        {/* Container */}
+        <aside className="statsContainer">
+          <StatsItem
+            title={'5000'}
+            text={
+              `Pessoas sensibilizadas 
+               atraves de eventos educacionais 
+               e partilha de experiencia`
+            }
+          />
+          <StatsItem
+            title={'+800'}
+            text={
+              `Pessoas sensibilizadas 
+               atraves de eventos educacionais 
+               e partilha de experiencia`
+            }
+          />
+          <StatsItem
+            title={'+800'}
+            text={
+              `Pessoas sensibilizadas 
+               atraves de eventos educacionais 
+               e partilha de experiencia`
+            }
+          />
+          <StatsItem
+            title={'+100'}
+            text={
+              `Pessoas sensibilizadas 
+               atraves de eventos educacionais 
+               e partilha de experiencia`
+            }
+          />
+        </aside>
+      </section>
+
+      {/* Learn Section */}
+      <section className="learn">
+        {/* Cover */}
+        <aside className="learnCover">
+          <Image
+            className="learnImage"
+            src={mozdevz}
+            alt='Comunidade Mozdevz'
+          />
+        </aside>
+
+        <aside className="learnDescription">
+          <h1 className="learnTittle">
+            Aprendemos em comunidade
+          </h1>
+          <p className="learnText">
+            Proporcionamos oportunidades de partilha de
+            conhecimento e desenvolvimento de habilidades
+            para a criação de produtos digitais a jovens
+            durante o period de formação e contribuímos
+            para o desenvolvimento da sociedade atraves
+            das soluções desenvolvidas.
+          </p>
+          {/* <Button>Saiba Mais</Button> */}
+        </aside>
+      </section>
+
+      <section className="connection">
+        {/* Description */}
+        <aside className="connectionDescription">
+          <h2 className="connectionTitle">
+            Conexão e Integração
+          </h2>
+          <span className="connectionHightlight">
+            Experiência - Tecnologia - Conexão
+          </span>
+        </aside>
+
+        {/* Container */}
+        <CardContainer />
+      </section>
+
+      {/* Sponsors */}
+      <section className="partners">
+        {/* Description */}
+        <aside className="partnersDescription">
+          <h2 className="partnersTitle">
+            Parceiros
+          </h2>
+          <span className="partnersHightlight">
+            Responsaveis pelo apoio e suporte da
+            comunidade
+          </span>
+        </aside> 
+
+        {/* Container */}
+        <LogoContainer /> 
+      </section>
+      <Footer />
+    </HomePage>
   )
 }
 ``
